47aacf4d0e9769db95818ac870ac8285eba59903
programarreza <programarreza@gmail.com>
2024-09-24 20:12:57 +0600

    added to item image gallery

--- src/app/(WithCommonLayout)/found-items/page.tsx
+++ src/app/(WithCommonLayout)/found-items/page.tsx
@@ -1,8 +1,19 @@
-const FoundItems = () => {
+import Container from "@/src/components/UI/Container";
+import Post from "@/src/components/UI/Post";
+import axiosInstance from "@/src/lib/AxiosInstance";
+import { IPost } from "@/src/types";
+
+const FoundItems = async () => {
+  const { data } = await axiosInstance.get(`/items`);
+
   return (
-    <div>
-      <h2>This is found items page</h2>
-    </div>
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        {data?.data?.map((post: IPost) => (
+          <Post key={post?._id} post={post} />
+        ))}
+      </div>
+    </Container>
   );
 };
 
